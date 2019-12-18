2df38dfbaa35320c0adea1130b68434aeb43b436
league <league@imac-21.attlocal.net>
2019-12-17 16:56:55 -0800

    Started fileIO

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,20 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
-	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+
+		// Create a program that takes a message from the user and saves it to a file.
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/text.txt");
+			fw.write(JOptionPane.showInputDialog("What to write to file??"));
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
