8196863b311cb33293a24fd160a72f036532af0d
Ryder Casazza <rydercasazza@me.com>
2019-12-21 16:24:47 -0800

    Finished level 5 module 0 except for pixelartmaker

---
+++ src/_02_Pixel_Art/PixelArtMaker.java
@@ -0,0 +1,99 @@
+package _02_Pixel_Art;
+
+import java.awt.FlowLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class PixelArtMaker implements MouseListener {
+	private JFrame window;
+	private GridInputPanel gip;
+	private GridPanel gp;
+	ColorSelectionPanel csp;
+	private JButton saveButton;
+	private String DATA_FILE = "src/_02_Pixel_Art/save.dat";
+
+	public void start() {
+		gip = new GridInputPanel(this);
+		window = new JFrame("Pixel Art");
+		window.setLayout(new FlowLayout());
+		window.setResizable(false);
+		saveButton = new JButton("Save");
+		window.add(gip);
+		window.pack();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		saveButton.addActionListener((e) -> save());
+	}
+
+	public void submitGridData(int w, int h, int r, int c) {
+		gp = load();
+		csp = new ColorSelectionPanel();
+		csp.add(saveButton);
+		window.remove(gip);
+		window.add(gp);
+		window.add(csp);
+		gp.repaint();
+		gp.addMouseListener(this);
+		window.pack();
+
+	}
+
+	private void save() {
+		try {
+			FileOutputStream fop = new FileOutputStream(DATA_FILE);
+			ObjectOutputStream oos = new ObjectOutputStream(fop);
+			oos.writeObject(gp);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private GridPanel load() {
+		try {
+			FileInputStream fis = new FileInputStream(DATA_FILE);
+			ObjectInputStream oos = new ObjectInputStream(fis);
+			return (GridPanel) oos.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		new PixelArtMaker().start();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		gp.setColor(csp.getSelectedColor());
+		System.out.println(csp.getSelectedColor());
+		gp.clickPixel(e.getX(), e.getY());
+		gp.repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+}
